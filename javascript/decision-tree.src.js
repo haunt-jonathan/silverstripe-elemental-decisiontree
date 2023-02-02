e017ac7468de6557f1b261dea1c361629eff738b
Jonathan Alpers <jonathan@hauntdigital.co.nz>
2023-02-03 12:58:11 +1300

    replaces jQuery with vanilla JS

--- javascript/decision-tree.src.js
+++ javascript/decision-tree.src.js
@@ -1,41 +1,87 @@
-(function($) {
+(function() {
 
-    $(document).ready(function() {
+    const serialize = function(formData) {
+        let str = Array.from(formData).map(entry => entry.map(encodeURIComponent).join("=")).join("&");
+        if (str) {
+            str += "&ajax=1";
+        }
+        return str;
+    }
+
+    const scrollTree = function(tree) {
+        var html = document.querySelector('html');
+        var body = document.querySelector('body');
+
+        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        var targetScroll = tree.getBoundingClientRect().top + currentScroll - 150;
+
+        var animateScroll = function(currentTime, start, change, duration) {
+            return (-change * (currentTime /= duration) * (currentTime - 2)) + start;
+        };
+
+        var duration = 500;
+        var start = currentScroll;
+        var change = targetScroll - start;
+        var startTime = performance.now();
+
+        var step = function() {
+            var currentTime = performance.now() - startTime;
+            if (currentTime < duration) {
+                var newScroll = animateScroll(currentTime, start, change, duration);
+                html.scrollTop = newScroll;
+                body.scrollTop = newScroll;
+                requestAnimationFrame(step);
+            } else {
+                html.scrollTop = targetScroll;
+                body.scrollTop = targetScroll;
+            }
+        };
+
+        requestAnimationFrame(step);
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
         /**
         * Submit Step form when selecting an answer
         * Adds loading animation to empty "nextstep" div
         * Fetch content via ajax
         * Insert HTML and update URL
         */
-        $(document).on( 'change', 'input[name=stepanswerid] ', function() {
-            var form = $(this).parents('form'),
-                step = form.parent('.step'),
-                nextstep_holder = step.find('> .nextstep');
+        
+        document.addEventListener('change', function(ev) {
+            var inputs = document.querySelectorAll('input[name=stepanswerid]');
+            if (Array.from(inputs).indexOf(ev.target) >= 0) {
+                var form = ev.target.closest('form'),
+                    step = form.closest('.step'),
+                    nextstep_holder = step.querySelector('.nextstep');
 
-                nextstep_holder.html('<div class="spinner-holder"><div class="spinner"><span class="sr-only">loading</span></div></div>');
-                setTimeout(function() {
-                    nextstep_holder.addClass('loading');
-                }, 100);
-
-                $.ajax({
-                    url     : form.attr('action'),
-                    type    : form.attr('method'),
-                    dataType: 'json',
-                    data    : form.serialize(),
-                    success : function( data ) {
-                        nextstep_holder.addClass('new-content-loaded');
-                        nextstep_holder.html(data.html);
-                        window.history.pushState(null, null, data.nexturl);
-                    },
-                    error   : function( xhr, err ) {
-                        nextstep_holder.html(xhr.responseText);
-                    }
-                }).always(function() {
+                    nextstep_holder.innerHTML = '<div class="spinner-holder"><div class="spinner"><span class="sr-only">loading</span></div></div>';
                     setTimeout(function() {
-                        nextstep_holder.removeClass('loading new-content-loaded');
+                        nextstep_holder.classList.add('loading');
                     }, 100);
-                });
-        });
+
+                    let url = form.getAttribute('action').split('?');
+                    if (url.length > 1) {
+                        url = url[0];
+                    }
+                    let xhr = new XMLHttpRequest();
+                    xhr.open('POST', url, true);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    xhr.responseType = 'json';
+                    xhr.send(serialize(new FormData(form)));
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            var data = xhr.response;
+                            nextstep_holder.classList.add('new-content-loaded');
+                            nextstep_holder.innerHTML = data.html;
+                            window.history.pushState(null, null, data.nexturl);
+                        } else {
+                            nextstep_holder.innerHTML = xhr.responseText;
+                        }
+                    };
+                }
+            }
+        );
 
         /**
         * Handles the restart button
@@ -43,27 +89,38 @@
         * Scroll back to first step then
         * Reset url to original page url
         */
-        $(document).on('click', 'button[data-action="restart-tree"]', function() {
-            var button = $(this),
-                firststep = button.parents('.step--first'),
-                radio = firststep.find('input[type="radio"]'),
-                tree = firststep.parents('.ElementDecisionTree');
+        document.addEventListener('click', function(ev) {
+            var buttons = document.querySelectorAll('button[data-action="restart-tree"]');
+            if (Array.from(buttons).indexOf(ev.target) >= 0) {
+                var button = ev.target,
+                firststep = button.closest('.step--first'),
+                radios = firststep.querySelectorAll('input[type="radio"]'),
+                tree = firststep.closest('.decisiontree');
 
-            if (firststep) {
-                firststep.find('.nextstep').fadeOut(function() {
-                    $(this).html('').show();
-                    radio.prop('checked', false);
+                if (firststep) {
 
-                    if ($(tree).length > 0) {
-                        $('html, body').animate({
-                            scrollTop: $(tree).offset().top - 150
-                        }, 500);
-                    }
-
-                    const url = location.protocol + '//' + location.host + location.pathname;
-                    window.history.pushState(null, null, url);
-                });
+                    var nextstep = firststep.querySelector('.nextstep');
+                    
+                    nextstep.style.opacity = 0;
+                    
+                    var fadeOutCallback = function() {
+                        nextstep.innerHTML = '';
+                        nextstep.style.display = 'block';
+                        nextstep.style.opacity = 1;
+                        radios.forEach(element => {
+                            element.checked = false; 
+                        });
+                        scrollTree(tree);
+        
+                        const url = location.protocol + '//' + location.host + location.pathname;
+                        window.history.pushState(null, null, url);
+                    };
+                    
+                    setTimeout(fadeOutCallback, 500);
+                    
+                }
+    
             }
         });
-    });
-})(jQuery);
+    }, false);
+})();
