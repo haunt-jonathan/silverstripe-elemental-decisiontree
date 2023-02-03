e9892aeaacd81bf38fb38be2e7423ec3b71c5103
Jonathan Alpers <jonathan@hauntdigital.co.nz>
2023-02-03 17:47:42 +1300

    debugging

--- javascript/decision-tree.src.js
+++ javascript/decision-tree.src.js
@@ -64,6 +64,9 @@
                     if (url.length > 1) {
                         url = url[0];
                     }
+                    if (! /\/$/.test(url)) {
+                        url += '/';
+                    }
                     let xhr = new XMLHttpRequest();
                     xhr.open('POST', url, true);
                     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
